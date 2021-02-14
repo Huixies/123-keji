db9bd5e76571cebb0331326c76cd6fa2d4d94006
Huixies <1062240360@qq.com>
2021-02-14 11:07:52 +0800

    移动端适配完成

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const headerEl = document.querySelector('header');
+const scrollToTop = document.querySelector('.scroll-to-top')
+
+window.addEventListener('scroll', () => {
+  let height = headerEl.getBoundingClientRect().height;
+
+  if (window.pageYOffset - height > 800) {
+    if (!headerEl.classList.contains('sticky')) {
+      headerEl.classList.add('sticky');
+    } 
+  }else {
+    headerEl.classList.remove('sticky');
+  }
+
+  if (window.pageYOffset > 2000) {
+    scrollToTop.style.display = 'block';
+  } else {
+    scrollToTop.style.display = 'none'
+  }
+})
+
 const glide = new Glide('.glide');
 const captionsEl = document.querySelectorAll('.slide-caption');
 
@@ -39,4 +60,65 @@ filterBtns.addEventListener('click', e => {
 
     isotope.arrange({ filter: filterOption });
   }
+});
+
+const staggeringOption = {
+  delay: 300,
+  distance: '50px',
+  duration: 500,
+  easing: 'ease-in-out',
+  origin:'bottom'
+}
+
+ScrollReveal().reveal('.feature', { ...staggeringOption, interval: 300 });
+ScrollReveal().reveal('.service-item', { ...staggeringOption, interval: 300 });
+
+const dataSectionEl = document.querySelector('.data-section');
+ScrollReveal().reveal('.data-section', {
+  beforeReveal: () => {
+    anime({
+      targets: '.data-piece .num',
+      innerHTML: el => {
+        return [0, el.innerHTML]
+      },
+      duration: 2500,
+      round: 1,
+      easing: "easeInExpo"
+    });
+    dataSectionEl.style.backgroundPosition = `center calc(50% - ${dataSectionEl.getBoundingClientRect().bottom / 5}px)`
+  }
+});
+
+window.addEventListener('scroll', () => {
+  const bottom = dataSectionEl.getBoundingClientRect().bottom;
+  const top = dataSectionEl.getBoundingClientRect().top;
+
+  if (bottom >= 0 && top <= window.innerHeight) {
+    dataSectionEl.style.backgroundPosition = `center calc(50% - ${bottom / 5}px)`
+  }
+});
+
+const scroll = new SmoothScroll("nav a[href*='#'], .scroll-to-top a[href*='#']", {
+  header: 'header',
+  offset:80
+})
+
+document.addEventListener('scrollStart', () => {
+  if (headerEl.classList.contains('open')) {
+    headerEl.classList.remove('open');
+  }
+})
+
+const exploreBtnEls = document.querySelectorAll('.explore-btn');
+
+exploreBtnEls.forEach(exploreBtnEl => {
+  exploreBtnEl.addEventListener('click', () => {
+    scroll.animateScroll(document.querySelector('#about-us'))
+  })
+})
+
+// 折叠按钮
+const burgerEl = document.querySelector('.burger');
+burgerEl.addEventListener('click', () => {
+  headerEl.classList.toggle('open')
 })
